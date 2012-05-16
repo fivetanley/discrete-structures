1b0924f0addef0cc4d6b09408ca1130daeda4a62
Stanley Stuart <dstanley.stuart@gmail.com>
2012-05-16 17:44:51 -0600

    Added non-integer test to factorial spec

--- specs/factorial_spec.rb
+++ specs/factorial_spec.rb
@@ -4,11 +4,14 @@ require 'src/factorial/factorial.rb'
 describe "factorial" do
 	# Factorials are calculated with non-negative integers:
 	# http://en.wikipedia.org/wiki/Factorial
-	it "should throw an exception when passed a non integer" do
+	it "should throw an exception when passed a non positive integer" do
 		# lambdas are like function objects in Ruby:
 		# http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 		# this example was taken from the RSpec book for
 		# raising errors ("throwing exceptions" for us Java people)
+		lambda {factorial(-1)}.should raise_errors(ArgumentError)
+	end
+	it "should throw an exception when passed a non integer" do
 		lambda {factorial(-1)}.should raise_errors(ArgumentError)
 	end
 end
