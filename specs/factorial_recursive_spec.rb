005cb9596e144d0fe5658a63a548ce2601be055a
Stanley Stuart <dstanley.stuart@gmail.com>
2012-05-16 18:44:19 -0600

    Added recursive implementation of factorial

---
+++ specs/factorial_recursive_spec.rb
@@ -0,0 +1,38 @@
+# Spec for factorials, implemented recursively
+require 'src/factorial/factorial_recursive.rb'
+
+describe "factorial" do
+	# Factorials are calculated with non-negative integers:
+	# http://en.wikipedia.org/wiki/Factorial
+	it "should throw an exception when passed a non positive integer" do
+		# lambdas are like function objects in Ruby:
+		# http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
+		# this example for writing/running/testing result of a lambda 
+		# was taken from the RSpec book raising errors (throwing exceptions)
+		# - The RSpec Book
+		lambda {factorial(-1)}.should raise_error(ArgumentError)
+	end
+
+	it "should throw an exception when passed a non integer" do
+		lambda {factorial(1.5)}.should raise_error(ArgumentError)
+	end
+
+	it "should return 1 when passed 0" do
+		result = factorial(0)
+		result.should == 1
+	end
+
+	it "should return the factorial of numbers less than or equal to
+		the passed integer" do
+		factorial_of_one = factorial(1)
+		factorial_of_two = factorial(2)
+		factorial_of_three = factorial(3)
+		factorial_of_four = factorial(4)
+		factorial_of_five = factorial(5)
+		factorial_of_one.should == 1
+		factorial_of_two.should == 2 * 1
+		factorial_of_three.should == 3 * 2 * 1
+		factorial_of_four.should == 4 * 3 * 2 * 1
+		factorial_of_five.should == 5 * 4 * 3 * 2 * 1
+	end
+end
---
+++ src/factorial/factorial_recursive.rb
@@ -0,0 +1,12 @@
+
+def factorial(positive_integer)
+	if positive_integer < 0
+		raise ArgumentError, "Cannot compute factorial of negative numbers."
+	elsif !positive_integer.integer?
+		raise ArgumentError, "Cannot compute factorial of non integers."
+	elsif positive_integer == 0
+		return 1
+	else
+		return positive_integer * factorial(positive_integer - 1)
+	end
+end
